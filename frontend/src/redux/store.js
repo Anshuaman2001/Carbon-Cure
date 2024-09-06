f4271df47d25a45c8cd9bbebd4cfe5d2a9f2ec34
Harsh Dhama <dhamaharsh9@gmail.com>
2024-09-06 19:12:33 +0530

    fixed store.js

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,11 +1,12 @@
-import { configureStore, applyMiddleware, combineReducers } from 'redux';
-import thunk from 'redux-thunk';
+import { configureStore } from '@reduxjs/toolkit';
+import { applyMiddleware, combineReducers } from 'redux';
+import { thunk } from 'redux-thunk';
 import { emissionReducer } from './reducers/emissionReducer';
 
 const rootReducer = combineReducers({
     emissionData: emissionReducer,
 });
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = configureStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
